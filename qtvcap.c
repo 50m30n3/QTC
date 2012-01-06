fdf687b6781085f6f48ab0954462752b1414f9cc
50m30n3 <someone@d00m.org>
2012-01-06 17:07:16 +0100

    Removed unused code from qtvcap

--- qtvcap.c
+++ qtvcap.c
@@ -28,46 +28,6 @@ unsigned long int get_time( void )
 	return tv.tv_sec * 1000000lu + tv.tv_usec;
 }
 
-int inc_filename( char *name )
-{
-	int i, carry, done;
-	
-	carry = 1;
-	done = 0;
-
-	for( i=strlen( name )-1; i>=0; i-- )
-	{
-		if( ( name[i] >= '0' ) && ( name[i] <= '9' ) )
-		{
-			done = 1;
-			if( carry )
-			{
-				if( name[i] < '9' )
-				{
-					name[i]++;
-					carry = 0;
-				}
-				else
-				{
-					name[i] = '0';
-					carry = 1;
-				}
-			}
-			else
-			{
-				break;
-			}
-		}
-		else
-		{
-			if( done )
-				break;
-		}
-	}
-	
-	return !carry;
-}
-
 int main( int argc, char *argv[] )
 {
 	struct image image, refimage;
