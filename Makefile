e841e766dca5b2447b5ab3b7885031a5ef67c460
50m30n3 <someone@d00m.org>
2012-01-08 18:42:06 +0100

    Split qtvcomp into seperate encoder and decoder

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-BINARIES = qtvcomp qtienc qtidec qtvcap
+BINARIES = qtienc qtidec qtvenc qtvdec qtvcap
 CC = gcc
 CFLAGS = -Wall -Wextra -O4 -march=native
 LDFLAGS = -lm
@@ -18,7 +18,8 @@ all: $(BINARIES)
 
 qtienc: qtienc.o databuffer.o image.o ppm.o qtc.o qti.o rangecode.o
 qtidec: qtidec.o databuffer.o image.o ppm.o qtc.o qti.o rangecode.o
-qtvcomp: qtvcomp.o databuffer.o image.o ppm.o qtc.o qti.o qtv.o rangecode.o
+qtvenc: qtvenc.o databuffer.o image.o ppm.o qtc.o qti.o qtv.o rangecode.o
+qtvdec: qtvdec.o databuffer.o image.o ppm.o qtc.o qti.o qtv.o rangecode.o
 
 qtvcap: qtvcap.o x11grab.o databuffer.o image.o qtc.o qti.o qtv.o rangecode.o
 	$(CC) $(CFLAGS) $(LDFLAGS) $(X11FLAGS) $^ -o $@
@@ -33,10 +34,10 @@ qtidec.o: qtidec.c image.h qti.h qtc.h ppm.h
 qtienc.o: qtienc.c image.h qti.h qtc.h ppm.h
 qtv.o: qtv.c databuffer.h rangecode.h qti.h qtv.h
 qtvcap.o: qtvcap.c image.h x11grab.h qti.h qtc.h qtv.h
-qtvcomp.o: qtvcomp.c image.h qti.h qtc.h qtv.h ppm.h
+qtvdec.o: qtvdec.c image.h qti.h qtc.h qtv.h ppm.h
+qtvenc.o: qtvenc.c image.h qti.h qtc.h qtv.h ppm.h
 rangecode.o: rangecode.c databuffer.h rangecode.h
 x11grab.o: x11grab.c image.h x11grab.h
-
 
 
 .PHONY: clean
---
+++ qtvdec.c
@@ -0,0 +1,215 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+
+#include "image.h"
+#include "qti.h"
+#include "qtc.h"
+#include "qtv.h"
+#include "ppm.h"
+
+int interrupt;
+
+void sig_exit( int sig )
+{
+	if( ( sig == SIGINT ) || ( sig == SIGTERM ) )
+		interrupt = 1;
+}
+
+int inc_filename( char *name )
+{
+	int i, carry, done;
+	
+	carry = 1;
+	done = 0;
+
+	for( i=strlen( name )-1; i>=0; i-- )
+	{
+		if( ( name[i] >= '0' ) && ( name[i] <= '9' ) )
+		{
+			done = 1;
+			if( carry )
+			{
+				if( name[i] < '9' )
+				{
+					name[i]++;
+					carry = 0;
+				}
+				else
+				{
+					name[i] = '0';
+					carry = 1;
+				}
+			}
+			else
+			{
+				break;
+			}
+		}
+		else
+		{
+			if( done )
+				break;
+		}
+	}
+	
+	return !carry;
+}
+
+int main( int argc, char *argv[] )
+{
+	struct image image, refimage;
+	struct qti compimage;
+	struct qtv video;
+
+	int opt, analyze;
+	int done, keyframe, framenum;
+	int startframe, numframes;
+	int qtw;
+	char *infile, *outfile;
+
+	analyze = 0;
+	startframe = 0;
+	numframes = -1;
+	qtw = 0;
+	infile = NULL;
+	outfile = NULL;
+
+	while( ( opt = getopt( argc, argv, "awf:n:i:o:" ) ) != -1 )
+	{
+		switch( opt )
+		{
+			case 'w':
+				qtw = 1;
+			break;
+
+			case 'a':
+				analyze = 1;
+			break;
+
+			case 'f':
+				if( sscanf( optarg, "%i", &startframe ) != 1 )
+					fputs( "ERROR: Can not parse command line\n", stderr );
+			break;
+
+			case 'n':
+				if( sscanf( optarg, "%i", &numframes ) != 1 )
+					fputs( "ERROR: Can not parse command line\n", stderr );
+			break;
+
+			case 'i':
+				infile = strdup( optarg );
+			break;
+
+			case 'o':
+				outfile = strdup( optarg );
+			break;
+
+			default:
+			case '?':
+				fputs( "ERROR: Can not parse command line\n", stderr );
+				return 1;
+			break;
+		}
+	}
+
+	interrupt = 0;
+	
+	signal( SIGINT, sig_exit );
+	signal( SIGTERM, sig_exit );
+
+	done = 0;
+	framenum = 0;
+
+	if( ! qtv_read_header( &video, qtw, infile ) )
+		return 0;
+
+	if( startframe != 0 )
+		qtv_seek( &video, startframe );
+
+	image_create( &refimage, video.width, video.height );
+
+	do
+	{
+		if( ! qtv_read_frame( &video, &compimage, &keyframe ) )
+			return 0;
+
+		if( !analyze )
+		{
+			if( keyframe )
+			{
+				if( compimage.colordiff >= 2 )
+				{
+					if( ! qtc_decompress_color_diff( &compimage, NULL, &image ) )
+						return 0;
+				}
+				else
+				{
+					if( ! qtc_decompress( &compimage, NULL, &image ) )
+						return 0;
+				}
+			}
+			else
+			{
+				if( compimage.colordiff >= 2 )
+				{
+					if( ! qtc_decompress_color_diff( &compimage, &refimage, &image ) )
+						return 0;
+				}
+				else
+				{
+					if( ! qtc_decompress( &compimage, &refimage, &image ) )
+						return 0;
+				}
+			}
+
+			image_copy( &image, &refimage );
+
+			if( compimage.transform == 1 )
+				image_transform_fast_rev( &image );
+			else if( compimage.transform == 2 )
+				image_transform_rev( &image );
+
+			if( compimage.colordiff >= 1 )
+				image_color_diff_rev( &image );
+
+		}
+		else
+		{
+			if( ! qtc_decompress_ccode( &compimage, &image, !keyframe ) )
+				return 0;	
+		}
+
+		if( ! ppm_write( &image, outfile ) )
+			return 0;
+
+		image_free( &image );
+		qti_free( &compimage );
+
+		if( ( outfile != NULL ) && ( strcmp( outfile, "-" ) != 0 ) )
+		{
+			if( !inc_filename( outfile ) )
+				done = 1;
+		}
+
+		if( interrupt )
+			done = 1;
+
+		if( ! qtv_can_read_frame( &video ) )
+			done = 1;
+
+		framenum++;
+	}
+	while( ( ! done ) && ( framenum != numframes ) );
+
+	image_free( &refimage );
+	qtv_free( &video );
+
+	free( infile );
+	free( outfile );
+
+	return 0;
+}
+
