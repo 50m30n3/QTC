4b9cba6017de5314a63d9419a8a48cff63d4013b
50m30n3 <someone@d00m.org>
2012-02-23 16:11:54 +0100

    Added license information

--- rangecode.c
+++ rangecode.c
@@ -1,15 +1,32 @@
+/*
+*    QTC: rangecode.c (c) 2011, 2012 50m30n3
+*
+*    The range coder in this file is based on the carry-less range coder
+*    by Dmitry Subbotin and the imlementation found here:
+*    http://www.sachingarg.com/compression/entropy_coding/64bit/
+*
+*    This file is part of QTC.
+*
+*    QTC is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    QTC is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with QTC.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 
 #include "databuffer.h"
 
 #include "rangecode.h"
-
-/*******************************************************************************
-* The range coder in this file is based on the carry-less range coder          *
-* by Dmitry Subbotin and the imlementation found here:                         *
-* http://www.sachingarg.com/compression/entropy_coding/64bit/                  *
-*******************************************************************************/
 
 unsigned const int maxrange = 0xFFFFFFFF;
 unsigned const int top = 0x01<<24;
