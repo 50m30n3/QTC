87e0ece2141735106ad17d762e6f41c4935bdf23
50m30n3 <someone@d00m.org>
2012-08-22 18:45:57 +0200

    Added some structures and files

---
+++ tilecache.c
@@ -0,0 +1,18 @@
+/*
+*    QTC: tilecache.c (c) 2011, 2012 50m30n3
+*
+*    This file is part of QTC.
+*
+*    QTC is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    QTC is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with QTC.  If not, see <http://www.gnu.org/licenses/>.
+*/
